12d39634ef07cd0debab7e7541251e6e130ccdde
rebecalazarini <160655787+rebecalazarini@users.noreply.github.com>
2024-03-05 15:58:51 -0300

    Update bilhões.c

--- bilhões.c
+++ bilhões.c
@@ -6,6 +6,6 @@ int main (){
     printf ("Digite dois números inteiros:");
     scanf("%lld %lld", &x, &y);
     printf("O número %lld multiplicado por 4 bilhôes é %lld", x, x * 4000000000LL);
-    printf("O número %lld multiplicado por 4 bilhôes é %lld", y, y * 2000000000LL);
+    printf("O número %lld multiplicado por 4 bilhôes é %lld", y, y * 4000000000LL);
     return 0;
 }
